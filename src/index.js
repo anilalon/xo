b6f59ba22b19f08b2f9c42c99c2b2e8ecf79e9eb
anilalon <anil.alon@gmail.com>
2020-03-08 12:24:05 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
 function Square(props) {
   return (
-    // eslint-disable-next-line react/prop-types
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
@@ -24,7 +22,6 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -49,23 +46,33 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{ squares: Array(9).fill(null) }],
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
       xIsNext: true
     };
   }
+
   handleClick(i) {
     const history = this.state.history;
     const current = history[history.length - 1];
-    const squares = this.state.squares.slice();
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      history: history.concat([{ squares: squares }]),
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
       xIsNext: !this.state.xIsNext
     });
   }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
@@ -75,7 +82,7 @@ class Game extends React.Component {
     if (winner) {
       status = "Winner: " + winner;
     } else {
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
     return (
@@ -84,13 +91,18 @@ class Game extends React.Component {
           <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/*status */}</div>
-          <ol> {/*todo */}</ol>
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
 }
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -110,5 +122,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-//==============================
-ReactDOM.render(<Game />, document.getElementById("root"));
